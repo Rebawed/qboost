a6aca733131b3010199ad97ecc8a0a900de8e5bb
Marco De Bartolomeo <marco.debartolomeo7@gmail.com>
2022-09-19 14:25:17 +0000

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os                                           #sudo apt-get install -y pyt
 from skimage.transform import resize
 from skimage.io import imread
 from skimage import color,data
-from skimage.filters import threshold_otsu
+#from skimage.filters import threshold_otsu
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,11 +24,11 @@ for i in Categories:
     for img in os.listdir(path):
         img_array=imread(os.path.join(path,img))
         imgGrayarray = color.rgb2gray(img_array)
-        
+        '''
         thresh = threshold_otsu(imgGrayarray)
         binary = imgGrayarray > thresh
-        
-        img_resized=resize(binary,(150,150,3))
+        '''
+        img_resized=resize(imgGrayarray,(150,150,3))
         flat_data_arr.append(img_resized.flatten())
         target_arr.append(Categories.index(i))
     print(f'loaded category:{i} successfully')
@@ -44,15 +44,19 @@ result = []
 for values in y:
     result.append(values * 2 - 1)
 
-X_train, x_test, y_train, y_test = train_test_split(X, result, train_size=0.8, test_size=0.2, shuffle=True)#stratify=y
-print('Splitted Successfully')
+x_train, x_test, y_train, y_test = train_test_split(X, result, train_size=0.8, test_size=0.2, shuffle=True)#stratify=y
+print('Number of features:', np.size(X, 1))
+print('Number of training samples:', len(x_train))
+print('Number of test samples:', len(x_test))
 
 #normalized_lambdas = np.linspace(0.0, 1.75, 10)
 #n_features = np.size(X, 1)
 #lambdas = normalized_lambdas / n_features
 #print('Performing cross-validation using {} values of lambda, this make take several minutes...'.format(len(lambdas)))
-#qboost, lam = qboost_lambda_sweep(X_train, y_train, lambdas, verbose=True)
+#qboost, lam = qboost_lambda_sweep(x_train, y_train, lambdas, verbose=True)
 
 lam = 0.4
-qboost= QBoostClassifier (X_train, y_train, lam, weak_clf_scale=None, drop_unused=True)
-report_baseline()
+qboost= QBoostClassifier (x_train, y_train, lam, weak_clf_scale=None, drop_unused=True)
+qboost.report_baseline(x_test,y_test)
+print('Number of selected features:',len(qboost.get_selected_features()))
+print('Score on test set: {:.3f}'.format(qboost.score(x_test, y_test)))
